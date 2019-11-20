df04d02cc9f0a84b06d010f335a115f33552be59
mhossen <mhossen@dealeron.com>
2019-11-20 13:21:14 -0500

    Added AutoFac for dependency Injection

---
+++ DependencyInjectionDemo/DemoLibrary/Utilities/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DemoLibrary.Utilities
+{
+  public class Logger : ILogger
+  {
+    public void Log(string message)
+    {
+      Console.WriteLine($"Logging { message }");
+    }
+
+  }
+}
